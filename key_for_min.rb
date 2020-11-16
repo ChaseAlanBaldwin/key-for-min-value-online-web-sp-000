c0eddbc27466cad2c7a86ac654aa7f1d86003a44
Chase-Alan Baldwin <alan.chasebaldwin@gmail.com>
2020-11-16 22:03:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   if name_hash.length < 1
     return nil
   end
-  lowestValue = 200000
+  lowestValue = nil
 
   name_hash.each do |key, value|
     if value < lowestValue
